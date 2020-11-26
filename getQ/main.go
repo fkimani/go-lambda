fbe525103b9ddc698894c86621732fdf9d2ab42c
Frank Maina <fmaina@Franks-MBP.hsd1.ga.comcast.net>
2020-11-26 17:50:58 -0500

    update getq

--- getQ/main.go
+++ getQ/main.go
@@ -1,14 +1,18 @@
 package main
 
-"github.com/aws-aws-lambda-go/events"
-"github.com/aws-aws-lambda-go/lambda"
+import (
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
 
 func main() {
 	lambda.Start(handler)
 }
 
 function handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error){
-
+	fmt.Println("Received body: ", request.Body)
 return request.APIGatewayProxyResponse(Body:"Hello World! ", StatusCode: 200), nil
 
 }
